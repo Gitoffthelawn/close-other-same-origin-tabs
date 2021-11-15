9f67c6eced9ecba8a1d2583791cf56f47323968f
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-11-15 15:02:00 +0100

    add toolbar button

--- background.js
+++ background.js
@@ -3,20 +3,25 @@ const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 const extdesc = manifest.description
 
+async function close_other_same_origin_tabs(tab){
+			const url = new URL(tab.url);
+			// close other visible tabs with same origin
+			const tabIds = (await browser.tabs.query({
+				url: url.origin + "/*",
+				hidden: false
+			})).filter(t => t.id !== tab.id).map(t => t.id);
+			browser.tabs.remove(tabIds);
+}
+
+browser.browserAction.onClicked.addListener( function(tab, clickData) {
+    close_other_same_origin_tabs(tab);
+});
+
 browser.menus.create({
 	id: extname,
 	title: extdesc,
-	contexts: ["tab"],
-	onclick: async function(info, tab) {
-		if(info.menuItemId.startsWith(extname)){
-			const url = new URL(tab.url);
-			// close other visible tabs with same origin
-			const tabIds = (await browser.tabs.query({ 
-				url: url.origin + "/*",
-				hidden: false 
-			})).filter(t => t.id !== tab.id).map(t => t.id);
-			browser.tabs.remove(tabIds);
-		}
-	}
+	contexts: ["tab","page"],
+    onclick: function(info, tab) {
+        close_other_same_origin_tabs(tab);
+    }
 });
-
